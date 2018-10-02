1eb386405278cee3879cb0fbd54b1de3caaafdea
Mark Mckittrick <mckittrickmark@gmail.com>
2018-10-02 18:05:33 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.set('view engine', 'ejs');
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.post("/urls", (req, res) => {
+  let templateVar = generateRandomString()
+  urlDatabase[templateVar] = req.body.longURL
+  console.log(urlDatabase);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls", (req, res) => {
+  var templateVars = { urls: urlDatabase };
+  //console.log(templateVars)
+  console.log(templateVars.urls)
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+  return Math.floor(Math.random() * 1000000 )
+}
